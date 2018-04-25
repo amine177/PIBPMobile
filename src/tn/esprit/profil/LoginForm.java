57e674701b604c2bdd10d1ea3213f58f65771766
Zain <zainelabidine.bensaleh@esprit.tn>
2018-04-25 09:19:55 +0100

    Res Without problems

---
+++ src/tn/esprit/profil/LoginForm.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tn.esprit.profil;
+import com.codename1.ui.Button;
+import com.codename1.ui.Container;
+import com.codename1.ui.Display;
+import com.codename1.ui.FontImage;
+import com.codename1.ui.Form;
+import com.codename1.ui.Image;
+import com.codename1.ui.Label;
+import com.codename1.ui.TextField;
+import com.codename1.ui.Toolbar;
+import com.codename1.ui.layouts.BorderLayout;
+import com.codename1.ui.layouts.BoxLayout;
+import com.codename1.ui.layouts.FlowLayout;
+import com.codename1.ui.util.Resources;
+
+
+public class LoginForm extends Form {
+    public LoginForm(Resources theme) {
+        super(new BorderLayout(BorderLayout.CENTER_BEHAVIOR_CENTER_ABSOLUTE));
+        setUIID("LoginForm");
+        Container welcome = FlowLayout.encloseCenter(
+                new Label("Bienvenue", ""),
+                new Label("", "WelcomeBlue")
+        );
+
+        getTitleArea().setUIID("Container");
+
+        Image profilePic = theme.getImage("user-picture.jpg");
+        System.out.println(profilePic==null);
+        Image mask = theme.getImage("round-mask.png");
+        System.out.println(mask==null);
+        profilePic = profilePic.fill(mask.getWidth(), mask.getHeight());
+        Label profilePicLabel = new Label(profilePic, "ProfilePic");
+        profilePicLabel.setMask(mask.createMask());
+        //TODO: Authentification
+        TextField login = new TextField("admin", "Login", 20,TextField.ANY) ;
+        TextField password = new TextField("admin", "Password", 20, TextField.PASSWORD) ;
+        login.getAllStyles().setMargin(LEFT, 0);
+        password.getAllStyles().setMargin(LEFT, 0);
+        Label loginIcon = new Label("", "TextField");
+        Label passwordIcon = new Label("", "TextField");
+        loginIcon.getAllStyles().setMargin(RIGHT, 0);
+        passwordIcon.getAllStyles().setMargin(RIGHT, 0);
+        FontImage.setMaterialIcon(loginIcon, FontImage.MATERIAL_PERSON_OUTLINE, 3);
+        FontImage.setMaterialIcon(passwordIcon, FontImage.MATERIAL_LOCK_OUTLINE, 3);
+
+        Button loginButton = new Button("LOGIN");
+        loginButton.setUIID("LoginButton");
+        loginButton.addActionListener(e -> {
+            Toolbar.setGlobalToolbar(false);
+            new WalkthruForm(theme).show();
+            Toolbar.setGlobalToolbar(true);
+        });
+
+        Button createNewAccount = new Button("CREATE NEW ACCOUNT");
+        createNewAccount.setUIID("CreateNewAccountButton");
+
+        // We remove the extra space for low resolution devices so things fit better
+        Label spaceLabel;
+        if(!Display.getInstance().isTablet() && Display.getInstance().getDeviceDensity() < Display.DENSITY_VERY_HIGH) {
+            spaceLabel = new Label();
+        } else {
+            spaceLabel = new Label(" ");
+        }
+
+
+        Container by = BoxLayout.encloseY(
+                welcome,
+                profilePicLabel,
+                spaceLabel,
+                BorderLayout.center(login).
+                        add(BorderLayout.WEST, loginIcon),
+                BorderLayout.center(password).
+                        add(BorderLayout.WEST, passwordIcon),
+                loginButton,
+                createNewAccount
+        );
+        add(BorderLayout.CENTER, by);
+
+        // for low res and landscape devices
+        by.setScrollableY(true);
+        by.setScrollVisible(false);
+    }
+}
