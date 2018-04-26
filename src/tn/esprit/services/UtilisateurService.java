4194475f45b9514212a680cac51aa98c75fad21c
Zain <zainelabidine.bensaleh@esprit.tn>
2018-04-26 23:54:52 +0100

    methode getAllAuthentificators

--- src/tn/esprit/services/UtilisateurService.java
+++ src/tn/esprit/services/UtilisateurService.java
@@ -5,7 +5,16 @@
  */
 package tn.esprit.services;
 
+import com.codename1.io.CharArrayReader;
+import com.codename1.io.ConnectionRequest;
+import com.codename1.io.JSONParser;
+import com.codename1.io.NetworkEvent;
+import com.codename1.io.NetworkManager;
+import com.codename1.ui.events.ActionListener;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import tn.esprit.entite.Utilisateur;
 
 /**
@@ -15,8 +24,40 @@ import tn.esprit.entite.Utilisateur;
 public class UtilisateurService {
 
     public ArrayList<Utilisateur> selectAllEnabled() {
-        return null;
+        ArrayList<Utilisateur> lstUsers = new ArrayList<>();
+        ConnectionRequest con = new ConnectionRequest();
+        con.setUrl("http://127.0.0.1:8000/profil/listAuthentificators/");
+        con.addResponseListener(new ActionListener<NetworkEvent>() {
+            @Override
+            public void actionPerformed(NetworkEvent evt) {
+                //listTasks = getListTask(new String(con.getResponseData()));
+                JSONParser jsonp = new JSONParser();
+                
+                try {
+                    //renvoi une map avec clé = root et valeur le reste
+                    Map<String, Object> users = jsonp.parseJSON(new CharArrayReader(new String(con.getResponseData()).toCharArray()));
+                    System.out.println("roooooot:" +users.get("root"));
 
+                    List<Map<String, Object>> list = (List<Map<String, Object>>) users.get("root");
+
+                    for (Map<String, Object> obj : list) {
+                        Utilisateur user = new Utilisateur();
+                        float id = Float.parseFloat(obj.get("id").toString());
+                        
+                        user.setId((int) id);
+                        /*user.setUsername(obj.get("username").toString());
+                        user.setUsernameCanonical("username_canonical");
+                        user.setNom(obj.get("name").toString());*/
+                        lstUsers.add(user);
+
+                    }
+                } catch (IOException ex) {
+                }
+
+            }
+        });
+        NetworkManager.getInstance().addToQueueAndWait(con);
+        return lstUsers;
     }
 
     
